d705b1a8ec71998a056531920c075fe470ace809
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-22 10:10:51 -0500

    Update to latest version of Dart.
    Convert RaisedButton to ElevatedButton.
    Convert FlatButton to TextButton.
    Add "const" where "required".
    Add Key? to constructors.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
@@ -10,11 +12,22 @@ class _LoadingScreenState extends State<LoadingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.blue,
+            elevation: 5,
+            padding: const EdgeInsets.all(12.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+          ),
           onPressed: () {
-            //Get the current location
+            // Get the current location
           },
-          child: Text('Get Location'),
+          child: const Text(
+            'Get Location',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
       ),
     );
